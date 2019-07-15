1ab81b03d43a72d691fa79c298792ffd41b7a27d
bugger279 <bugger279@gmail.com>
2019-07-15 18:10:31 +0530

    Hover Transition effect

---
+++ js/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: '../images/tom1.jpeg',
+    image2: '../images/tom2.jpeg',
+    displacementImage: '../images/heightMap.png'
+});
